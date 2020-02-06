1ef676c5d4178f42d1776cc1a8912425b4369a73
Jarchilamurillo <45294299+Jarchilamurillo@users.noreply.github.com>
2020-02-05 18:03:18 -0600

    Cambios varios
    Cambios varios

--- Labs_digital_2.X/LAB_2.c
+++ Labs_digital_2.X/LAB_2.c
@@ -11,19 +11,19 @@
 
 // CONFIG1
 #pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
-#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
-#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
-#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+#pragma config WDTE = OFF           // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF          // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF          // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF             // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF            // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF          // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF           // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF          // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF            // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
 
 // CONFIG2
-#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+#pragma config BOR4V = BOR40V       // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF            // Flash Program Memory Self Write Enable bits (Write protection off)
 
 #define _XTAL_FREQ  8000000
 #define __delay_ms(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000.0)))
@@ -32,62 +32,93 @@
 // Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
+#include <stdint.h>
 
 
-unsigned char contador = 0;
+uint8_t contador = 0;
 //variable de boton de suma y suma
 char IN1 = 0;
 char INN1 = 0;
 //variable de boton de resta y resta
 char IN2 = 0;
 char INN2 = 0;
+//Array para display (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F)
+unsigned char disp [] = {0b11101110, 0b00101000, 0b11001101, 0b01101101, 0b00101011, 
+0b01100111, 0b11100111, 0b00101100, 0b11101111, 0b01101111, 0b10101111, 0b11100011, 
+0b11000110, 0b11101001, 0b11000111, 0b10000111};
 
-void __interrupt() contadorrr(void){
+void __interrupt() ISR(void){
     
-    if(PORTDbits.RD3==1){
+    if(PORTBbits.RB1==1){
         IN1=1;
     }
-    if(PORTDbits.RD3==0 && IN1==1){
+    if(PORTBbits.RB1==0 && IN1==1){
+        di();
         IN1=0;
         INN1=1;
+        ei();
     }
-    if(PORTDbits.RD2==1){
+    if(PORTBbits.RB0==1){
         IN2 = 1;
     }
-    if(PORTDbits.RD2==0 && IN2==1){
+    if(PORTBbits.RB0==0 && IN2==1){
+        di();
         IN2=0;
         INN2=1;
+        ei();
     }
 }
 
 void main(void){
     //Entradas/salidas
     TRISC = 0;
+    TRISA = 0;
     //Botones
-    TRISDbits.TRISD2 = 1;
-    TRISDbits.TRISD3 = 1;
+    TRISBbits.TRISB0 = 1;
+    TRISBbits.TRISB1 = 1;
+    IOCBbits.IOCB0 = 1;
+    IOCBbits.IOCB1 = 1;
+    //anteriormente en d2 y d3
+    
     //Transistores
     TRISDbits.TRISD4 = 0;
     TRISDbits.TRISD5 = 0;
     //Entrada analógica
     TRISBbits.TRISB5 = 1;
+    ANSELH = 0b00100000;
     //LED alarma
     TRISBbits.TRISB7 = 0;
+    
     //Seteando ADC a un rate de Fosc/32
     ADCON0bits.ADCS1 = 1;
     ADCON0bits.ADCS0 = 0;
     //Escogiendo canal del ADC
     ADCON0bits.CHS = 0b1101;
-    //Formato del resultado (derecha)
-    ADCON1bits.ADFM = 1;
+    //Enable ADC
+    ADCON0bits.ADON = 1;
+    //Formato del resultado (izquierda)
+    ADCON1bits.ADFM = 0;
+    //Pines de v ref
+    ADCON1bits.VCFG1 = 0;
+    ADCON1bits.VCFG0 = 0;
+    
     //Seteando reloj interno a 8MHz
     OSCCONbits.IRCF0 = 1;
     OSCCONbits.IRCF1 = 1;
     OSCCONbits.IRCF2 = 1;
     //Mas bits para ADC
     ADCON0bits.GO_nDONE = 1;
-    //Enable ADC
-    ADCON0bits.ADON = 1;
+    
+    //TMRO
+    OPTION_REGbits.nRBPU = 1;
+    OPTION_REGbits.INTEDG = 0;
+    OPTION_REGbits.T0CS = 0;
+    OPTION_REGbits.T0SE = 0;
+    OPTION_REGbits.PSA = 0;
+    OPTION_REGbits.PS = 0b000;
+    TMR0 = 3;
+    
+    
     //Estados iniciales
     PORTDbits.RD4 = 0;
     PORTDbits.RD5 = 0;
@@ -95,7 +126,7 @@ void main(void){
     PORTBbits.RB7 = 0;
     
     while(1){
-        PORTC = ADRESL;
+        PORTC = ADRESH;
         PORTDbits.RD4 = 1;
         __delay_ms(10);
         PORTDbits.RD4 = 0;
@@ -104,6 +135,8 @@ void main(void){
         PORTDbits.RD5 = 1;
         __delay_ms(10);
         PORTDbits.RD5 = 0;
+        
+        PORTA = contador;
         
         if(ADCON0bits.ADON==0){
             ADCON0bits.ADON = 1;
